6d0eb4cdbf31e5a40a67bf8a94a5557abe1647af
Renat Zubairov <renat.zubairov@gmail.com>
2017-08-28 16:11:32 +0200

    Fixed issue#1

--- spec/transform.spec.js
+++ spec/transform.spec.js
@@ -4,16 +4,30 @@ const transform = require('../lib/actions/transform');
 const eioUtils = require('elasticio-node').messages;
 
 describe('Transformation test', () => {
-  it('should handle simple transforms', () => {
-    return transform.process(eioUtils.newMessageWithBody({
-      first: 'Renat',
-      last: 'Zubairov'
-    }), {
-      expression: `{ "fullName": first & " " & last }`
-    }).then(result => {
-      expect(result.body).to.deep.equal({
-        fullName: 'Renat Zubairov'
-      });
+
+    it('should handle simple transforms', () => {
+        return transform.process(eioUtils.newMessageWithBody({
+            first: 'Renat',
+            last: 'Zubairov'
+        }), {
+            expression: `{ "fullName": first & " " & last }`
+        }).then(result => {
+            expect(result.body).to.deep.equal({
+                fullName: 'Renat Zubairov'
+            });
+        });
     });
-  });
+
+    it('should not produce an empty message if transformation returns undefined', () => {
+        return transform.process(eioUtils.newMessageWithBody({
+            first: 'Renat',
+            last: 'Zubairov'
+        }), {
+            expression: `$[foo=2].({ "foo": boom })`
+        }).then(result => {
+            expect(result).to.be.an('undefined');
+        });
+    });
+
+
 });
