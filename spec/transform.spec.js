6eb5dca22991f030406d51b23469b862acc74410
Hannah Bulmer <hannah.bulmer@elastic.io>
2019-09-12 14:17:58 +0200

    Added testing for checkbox

--- spec/transform.spec.js
+++ spec/transform.spec.js
@@ -3,8 +3,23 @@
 const expect = require('chai').expect;
 const transform = require('../lib/actions/transform');
 const eioUtils = require('elasticio-node').messages;
+const sinon = require('sinon');
+
+const testData = {
+  phone: [
+    '12345678', '87654321'
+  ]
+};
 
 describe('Transformation test', () => {
+  let self;
+
+  beforeEach(() => {
+    self = {
+      emit: sinon.spy()
+    };
+  });
+
   it('should handle simple transforms', () => {
     return transform.process(eioUtils.newMessageWithBody({
       first: 'Renat',
@@ -26,6 +41,34 @@ describe('Transformation test', () => {
       expression: `$[foo=2].({ "foo": boom })`
     }).then(result => {
       expect(result).to.be.an('undefined');
+    });
+  });
+
+  it('should return an array when the checkbox is selected', () => {
+    return transform.process(eioUtils.newMessageWithBody(testData), {
+      expression: 'phone',
+      dontSplitArray: true
+    }).then(result => {
+      expect(result.body).to.deep.equal([
+        '12345678', '87654321'
+      ]);
+    });
+  });
+
+  it('should return multiple objects when checkbox is deselected', async () => {
+    return transform.process.call(self, eioUtils.newMessageWithBody(testData), {
+      expression: 'phone',
+      dontSplitArray: false
+    }).then(() => {
+      expect(self.calledTwice); // emits twice
+    });
+  });
+
+  it('should return multiple objects when checkbox field is undefined', async () => {
+    return transform.process.call(self, eioUtils.newMessageWithBody(testData), {
+      expression: 'phone'
+    }).then(() => {
+      expect(self.calledTwice); // emits twice
     });
   });
 
