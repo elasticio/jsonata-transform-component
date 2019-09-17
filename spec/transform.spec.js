bcf478e0a3ac2b24ed0b1c5b17e3b2d5b28fed52
Hannah Bulmer <hannah.bulmer@elastic.io>
2019-09-17 10:15:00 +0200

    Add eslinting, improve unit tests

--- spec/transform.spec.js
+++ spec/transform.spec.js
@@ -1,14 +1,12 @@
-/* eslint-disable arrow-body-style */
-
-const expect = require('chai').expect;
-const transform = require('../lib/actions/transform');
+const { expect } = require('chai');
 const eioUtils = require('elasticio-node').messages;
 const sinon = require('sinon');
+const transform = require('../lib/actions/transform');
 
 const testData = {
   phone: [
-    '12345678', '87654321'
-  ]
+    '12345678', '87654321',
+  ],
 };
 
 describe('Transformation test', () => {
@@ -16,75 +14,67 @@ describe('Transformation test', () => {
 
   beforeEach(() => {
     self = {
-      emit: sinon.spy()
+      emit: sinon.spy(),
     };
   });
 
-  it('should handle simple transforms', () => {
-    return transform.process(eioUtils.newMessageWithBody({
-      first: 'Renat',
-      last: 'Zubairov'
-    }), {
-      expression: `{ "fullName": first & " " & last }`
-    }).then(result => {
-      expect(result.body).to.deep.equal({
-        fullName: 'Renat Zubairov'
-      });
+  it('should handle simple transforms', () => transform.process.call(self, eioUtils.newMessageWithBody({
+    first: 'Renat',
+    last: 'Zubairov',
+  }), {
+    expression: '{ "fullName": first & " " & last }',
+  }).then(() => {
+    expect(self.emit.args[0][1].body).to.deep.equal({
+      fullName: 'Renat Zubairov',
     });
-  });
+  }));
 
-  it('should not produce an empty message if transformation returns undefined', () => {
-    return transform.process(eioUtils.newMessageWithBody({
-      first: 'Renat',
-      last: 'Zubairov'
-    }), {
-      expression: `$[foo=2].({ "foo": boom })`
-    }).then(result => {
-      expect(result).to.be.an('undefined');
-    });
-  });
+  it('should not produce an empty message if transformation returns undefined', () => transform.process.call(self, eioUtils.newMessageWithBody({
+    first: 'Renat',
+    last: 'Zubairov',
+  }), {
+    expression: '$[foo=2].({ "foo": boom })',
+  }).then(() => {
+    expect(self.emit.called).to.be.equal(false);
+  }));
 
-  it('should return an array when the checkbox is selected', () => {
-    return transform.process(eioUtils.newMessageWithBody(testData), {
-      expression: 'phone',
-      dontSplitArray: true
-    }).then(result => {
-      expect(result.body).to.deep.equal([
-        '12345678', '87654321'
-      ]);
-    });
-  });
+  it('should return an array when the checkbox is selected', () => transform.process.call(self, eioUtils.newMessageWithBody(testData), {
+    expression: 'phone',
+    dontSplitArray: true,
+  }).then(() => {
+    expect(self.emit.args[0][1].body).to.deep.equal([
+      '12345678', '87654321',
+    ]);
+  }));
 
-  it('should return multiple objects when checkbox is deselected', async () => {
-    return transform.process.call(self, eioUtils.newMessageWithBody(testData), {
-      expression: 'phone',
-      dontSplitArray: false
-    }).then(() => {
-      expect(self.calledTwice); // emits twice
-    });
-  });
+  it('should return multiple objects when checkbox is deselected', async () => transform.process.call(self, eioUtils.newMessageWithBody(testData), {
+    expression: 'phone',
+    dontSplitArray: false,
+  }).then(() => {
+    expect(self.emit.calledTwice).to.be.equal(true); // emits twice
+    expect(self.emit.args[0][1].body).to.deep.equal('12345678');
+    expect(self.emit.args[1][1].body).to.deep.equal('87654321');
+  }));
 
-  it('should return multiple objects when checkbox field is undefined', async () => {
-    return transform.process.call(self, eioUtils.newMessageWithBody(testData), {
-      expression: 'phone'
-    }).then(() => {
-      expect(self.calledTwice); // emits twice
-    });
-  });
+  it('should return multiple objects when checkbox field is undefined', async () => transform.process.call(self, eioUtils.newMessageWithBody(testData), {
+    expression: 'phone',
+  }).then(() => {
+    expect(self.emit.calledTwice).to.be.equal(true); // emits twice
+  }));
 
   it('should handle passthough properly', () => {
     const msg = eioUtils.newMessageWithBody({
       first: 'Renat',
-      last: 'Zubairov'
+      last: 'Zubairov',
     });
     msg.passthrough = {
-      ps: 'psworks'
+      ps: 'psworks',
     };
-    return transform.process(msg, {
-      expression: `{ "fullName": first & " " & elasticio.ps}`
-    }).then(result => {
-      expect(result.body).to.deep.equal({
-        fullName: 'Renat psworks'
+    return transform.process.call(self, msg, {
+      expression: '{ "fullName": first & " " & elasticio.ps}',
+    }).then(() => {
+      expect(self.emit.args[0][1].body).to.deep.equal({
+        fullName: 'Renat psworks',
       });
     });
   });
