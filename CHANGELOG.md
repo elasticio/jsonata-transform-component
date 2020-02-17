e7805d3fba913f0421f1916525045d6d2bcb7813
ElBabbler <gladikov.denis@gmail.com>
2020-02-17 15:15:02 +0200

    Fix codestyle and eslint version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 1.0.3 (February 17, 2020)
+
+* Fix passthrough bug
+* Fix codestyle
+
 ## 1.0.2 (December 24, 2019)
 
 * Update sailor version to 2.5.4
--- spec/transform.spec.js
+++ spec/transform.spec.js
@@ -1,50 +1,43 @@
-/* eslint-disable arrow-body-style */
+const { expect } = require('chai');
+const { messages } = require('elasticio-node');
+const logger = require('@elastic.io/component-logger')();
 
-const {expect} = require('chai');
 const transform = require('../lib/actions/transform');
-const {messages} = require('elasticio-node');
-const bunyan = require('bunyan');
-
-const logger = bunyan.createLogger({name: 'dummy'});
 
 describe('Transformation test', () => {
-  it('should handle simple transforms', () => {
-    return transform.process.call({logger}, messages.newMessageWithBody({
-      first: 'Renat',
-      last: 'Zubairov'
-    }), {
-      expression: `{ "fullName": first & " " & last }`
-    }).then(result => {
-      expect(result.body).to.deep.equal({
-        fullName: 'Renat Zubairov'
-      });
+  it('should handle simple transforms', () => transform.process.call({ logger }, messages.newMessageWithBody({
+    first: 'Renat',
+    last: 'Zubairov',
+  }), {
+    expression: '{ "fullName": first & " " & last }',
+  }).then((result) => {
+    expect(result.body).to.deep.equal({
+      fullName: 'Renat Zubairov',
     });
-  });
+  }));
 
-  it('should not produce an empty message if transformation returns undefined', () => {
-    return transform.process.call({logger}, messages.newMessageWithBody({
-      first: 'Renat',
-      last: 'Zubairov'
-    }), {
-      expression: `$[foo=2].({ "foo": boom })`
-    }).then(result => {
-      expect(result).to.be.an('undefined');
-    });
-  });
+  it('should not produce an empty message if transformation returns undefined', () => transform.process.call({ logger }, messages.newMessageWithBody({
+    first: 'Renat',
+    last: 'Zubairov',
+  }), {
+    expression: '$[foo=2].({ "foo": boom })',
+  }).then((result) => {
+    expect(result).to.be.an('undefined');
+  }));
 
   it('should handle passthough properly', () => {
     const msg = messages.newMessageWithBody({
       first: 'Renat',
-      last: 'Zubairov'
+      last: 'Zubairov',
     });
     msg.passthrough = {
-      ps: 'psworks'
+      ps: 'psworks',
     };
-    return transform.process.call({logger}, msg, {
-      expression: `{ "fullName": first & " " & elasticio.ps}`
-    }).then(result => {
+    return transform.process.call({ logger }, msg, {
+      expression: '{ "fullName": first & " " & elasticio.ps}',
+    }).then((result) => {
       expect(result.body).to.deep.equal({
-        fullName: 'Renat psworks'
+        fullName: 'Renat psworks',
       });
     });
   });
